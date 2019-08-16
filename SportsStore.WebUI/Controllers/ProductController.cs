97a50de9c11e11f2bf4b3b7d927b5a047780fcef
Shuhratzhan Nizamov <shukhratzhon.nizamov@nat.kz>
2019-08-16 10:23:45 +0600

    Initialization Solution with testing, domain, web

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize = 4;
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _repository = productRepository;
+        }
+
+        public ViewResult List(int page=1)
+        {
+            ProductsListViewModel viewModel = new ProductsListViewModel
+                                               {
+                                                   Products = _repository.Products
+                                                   .OrderBy(p => p.ProductID)
+                                                   .Skip((page - 1) * PageSize)
+                                                   .Take(PageSize),
+                                                   PagingInfo = new PagingInfo
+                                                                    {
+                                                                        CurrentPage = page,
+                                                                        ItemsPerPage = PageSize,
+                                                                        TotalItems = _repository.Products.Count()
+                                                                    }
+                                               };
+            return View(viewModel);
+        }
+
+    }
+}

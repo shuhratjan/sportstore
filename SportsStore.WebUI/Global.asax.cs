3993622e56b52a267808cae250c70bc16b786dfc
Shuhratzhan Nizamov <shukhratzhon.nizamov@nat.kz>
2019-08-16 11:41:41 +0600

    Fix Route

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -22,19 +22,28 @@ namespace SportsStore.WebUI
 
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(null, "", 
+                new {
+                    controller = "Product", action = "List",
+                    category = (string) null, page = 1
+                });
 
-            routes.MapRoute(
-                null,
-                "Page{page}",
-                new {Controller = "Product", action="List"}
+            routes.MapRoute(null, "Page{page}",
+                            new {controller = "Product", action = "List", category = (string) null},
+                            new {page = @"\d+"}
+                            );
+
+            routes.MapRoute(null, "{category}", 
+                new { controller = "Product", action = "List", page = 1}
                 );
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Product", action = "List", id = UrlParameter.Optional } // Parameter defaults
-            );
 
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Product", action = "List"},
+                new {page =@"\d+"}
+                );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
 
         protected void Application_Start()

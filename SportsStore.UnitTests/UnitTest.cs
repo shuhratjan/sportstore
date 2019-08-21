6a1ad77f876d7b7e914b9b43dad1d68d9912aa5b
Shuhratzhan Nizamov <shukhratzhon.nizamov@nat.kz>
2019-08-21 11:49:28 +0600

    protecting administrative features, added Security Authentication functionality only for one user, for admin.
    Also added new unit testing methods to check Auth features

--- SportsStore.UnitTests/UnitTest.cs
+++ SportsStore.UnitTests/UnitTest.cs
@@ -8,6 +8,7 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
 using SportsStore.WebUI.HtmlHelpers;
+using SportsStore.WebUI.Infrastructure.Abstract;
 using SportsStore.WebUI.Models;
 
 namespace SportsStore.UnitTests
@@ -484,6 +485,47 @@ namespace SportsStore.UnitTests
             mock.Verify(m=> m.DeleteProduct(It.IsAny<Product>()),Times.Never());
 
         }
+
+        [TestMethod]
+        public void Can_Login_With_Valid_Credentials()
+        {
+            Mock<IAuthProvider>mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("admin", "secret")).Returns(true);
+
+            LogOnViewModel model = new LogOnViewModel
+                                       {
+                                           UserName = "admin",
+                                           Password = "secret"
+                                       };
+
+            AccountController target = new AccountController(mock.Object);
+
+            ActionResult result = target.LogOn(model, "/MyURL");
+
+            Assert.IsInstanceOfType(result, typeof(RedirectResult));
+            Assert.AreEqual("/MyURL", ((RedirectResult)result).Url);
+        }
+
+        [TestMethod]
+        public void Cannot_Login_With_Invalid_Credentials()
+        {
+            Mock<IAuthProvider> mock = new Mock<IAuthProvider>();
+            mock.Setup(m => m.Authenticate("badUser", "badPass")).Returns(false);
+
+            LogOnViewModel model = new LogOnViewModel
+            {
+                UserName = "badUser",
+                Password = "badPass"
+            };
+
+            AccountController target = new AccountController(mock.Object);
+
+            ActionResult result = target.LogOn(model, "/MyURL");
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.IsFalse(((ViewResult)result).ViewData.ModelState.IsValid);
+        }
+
 
 
     }
